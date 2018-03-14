838929ec4bd9c7e7656b730ecabb699ca70f83e8
zjun <565061763@qq.com>
2018-03-14 13:48:47 +0800

    demo

--- app/src/main/java/com/fanwe/www/http/AppRequestInterceptor.java
+++ app/src/main/java/com/fanwe/www/http/AppRequestInterceptor.java
@@ -6,6 +6,9 @@ import com.fanwe.lib.http.IRequest;
 import com.fanwe.lib.http.IResponse;
 import com.fanwe.lib.http.interceptor.IRequestInterceptor;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 /**
  * http请求拦截
  */
@@ -13,10 +16,13 @@ public class AppRequestInterceptor implements IRequestInterceptor
 {
     public static final String TAG = "AppRequestInterceptor";
 
+    private final Map<IRequest, Long> mMapTime = new WeakHashMap<>();
+
     @Override
     public void beforeExecute(IRequest request)
     {
         //请求发起之前回调
+        mMapTime.put(request, System.currentTimeMillis());
         Log.i(TAG, "beforeExecute:" + request);
     }
 
@@ -24,6 +30,7 @@ public class AppRequestInterceptor implements IRequestInterceptor
     public void afterExecute(IRequest request, IResponse response)
     {
         //请求发起之后回调
-        Log.i(TAG, "afterExecute:" + request);
+        long time = System.currentTimeMillis() - mMapTime.get(request);
+        Log.i(TAG, "afterExecute:" + request + " " + time);
     }
 }
