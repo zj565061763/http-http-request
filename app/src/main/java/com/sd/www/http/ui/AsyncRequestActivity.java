b2855ee64918315d7967287c8883f8b4bdbf8c7c
zjun <565061763@qq.com>
2018-08-31 10:27:23 +0800

    demo

--- app/src/main/java/com/sd/www/http/ui/AsyncRequestActivity.java
+++ app/src/main/java/com/sd/www/http/ui/AsyncRequestActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.google.gson.Gson;
 import com.sd.lib.http.IRequest;
 import com.sd.lib.http.IResponse;
+import com.sd.lib.http.RequestHandler;
 import com.sd.lib.http.RequestManager;
 import com.sd.lib.http.callback.ModelRequestCallback;
 import com.sd.lib.http.impl.httprequest.GetRequest;
@@ -32,71 +33,84 @@ public class AsyncRequestActivity extends AppCompatActivity
     public void onClickRequest(View view)
     {
         IRequest request = new GetRequest();
-        request.setBaseUrl(URL); //设置请求地址
-        request.getParams().put("aaa", "aaa").put("bbb", "bbb"); //设置请求参数
-        request.setTag(TAG); //设置该请求的tag，可用于取消请求
+        // 设置请求地址
+        request.setBaseUrl(URL);
+        // 设置请求参数
+        request.getParams().put("aaa", "aaa").put("bbb", "bbb");
+        // 设置该请求的tag，可用于取消请求
+        request.setTag(TAG);
 
-        request.execute(mModelRequestCallback);
+        // 发起异步请求
+        RequestHandler requestHandler = request.execute(new ModelRequestCallback<WeatherModel>()
+        {
+            @Override
+            public void onPrepare(IRequest request)
+            {
+                super.onPrepare(request);
+                // 请求在被执行之前的准备回调(发起请求被调用的线程)
+                Log.i(TAG, "onPrepare");
+            }
+
+            @Override
+            public void onStart()
+            {
+                super.onStart();
+                // 请求开始执行(UI线程)
+                Log.i(TAG, "onStart");
+            }
+
+            @Override
+            public void onSuccessBackground() throws Exception
+            {
+                super.onSuccessBackground();
+                // 成功回调，super里面回调了parseToModel方法把返回的内容转为实体，(非UI线程)
+                Log.i(TAG, "onSuccessBackground");
+            }
+
+            @Override
+            protected WeatherModel parseToModel(String content, Class<WeatherModel> clazz)
+            {
+                // 把返回的内容转实体(非UI线程)
+                return new Gson().fromJson(content, clazz);
+            }
+
+            @Override
+            public void onSuccess()
+            {
+                // 成功回调(UI线程)
+
+                // 获得返回结果对象
+                IResponse response = getResponse();
+                // 获得接口对应的实体
+                WeatherModel model = getActModel();
+                Log.i(TAG, "onSuccess:" + model.weatherinfo.city);
+            }
+
+            @Override
+            public void onError(Exception e)
+            {
+                super.onError(e);
+                // 异常回调，请求异常或者转实体出错(UI线程)
+                Log.i(TAG, "onError:" + e);
+            }
+
+            @Override
+            public void onCancel()
+            {
+                super.onCancel();
+                // 请求被取消回调(UI线程)
+                Log.i(TAG, "onCancel");
+            }
+
+            @Override
+            public void onFinish()
+            {
+                super.onFinish();
+                // 结束回调(UI线程)
+                Log.i(TAG, "onFinish");
+            }
+        });
     }
-
-    private final ModelRequestCallback mModelRequestCallback = new ModelRequestCallback<WeatherModel>()
-    {
-        @Override
-        public void onPrepare(IRequest request)
-        {
-            super.onPrepare(request);
-            Log.i(TAG, "onPrepare_0");
-        }
-
-        @Override
-        public void onStart()
-        {
-            super.onStart();
-            Log.i(TAG, "onStart_0");
-        }
-
-        @Override
-        public void onSuccessBackground() throws Exception
-        {
-            super.onSuccessBackground();
-            Log.i(TAG, "onSuccessBackground_0");
-        }
-
-        @Override
-        protected WeatherModel parseToModel(String content, Class<WeatherModel> clazz)
-        {
-            return new Gson().fromJson(content, clazz);
-        }
-
-        @Override
-        public void onSuccess()
-        {
-            IResponse response = getResponse(); //获得返回结果对象
-            WeatherModel model = getActModel(); // 获得接口对应的实体
-            Log.i(TAG, "onSuccess_0:" + model.weatherinfo.city);
-        }
-
-        @Override
-        public void onError(Exception e)
-        {
-            super.onError(e);
-            Log.i(TAG, "onError_0:" + e);
-        }
-
-        @Override
-        public void onCancel()
-        {
-            super.onCancel();
-            Log.i(TAG, "onCancel_0");
-        }
-
-        @Override
-        public void onFinish()
-        {
-            super.onFinish();
-            Log.i(TAG, "onFinish_0");
-        }
-    };
 
     public void onClickCancelRequest(View view)
     {
